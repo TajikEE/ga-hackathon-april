534b31344392bb40f6f64b1b6d48246bfe267a2a
Tajik <tajik.ifrad@gmail.com>
2022-04-28 16:32:09 +0300

    fix

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
-// import {
-//   WebSocketClient,
-//   WebSocketServer,
-// } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
 import { sentences, randomId } from "./data.ts";
 
 const app = new Application();
@@ -14,25 +10,22 @@ app.use(
   })
 );
 
-
-
 const router = new Router();
 router
   .get("/", (context) => {
     context.response.body = "Hello world!";
   })
   .get("/sentences", (context) => {
-    const res = {id: randomId(), sentences}
+    const res = { id: randomId(), sentences };
     context.response.body = res;
   });
 
-// const wss = new WebSocketServer(8080);
-// wss.on("connection", function (ws: WebSocketClient) {
-//   ws.on("message", function (message: string) {
-//     console.log(message);
-//     ws.send(message);
-//   });
-// });
+router.get("/ws", async (ctx) => {
+  const sock = await ctx.upgrade();
+  console.log(sock);
+
+  return sock
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
