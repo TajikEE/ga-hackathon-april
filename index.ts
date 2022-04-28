592a2775054113681962256d6fb0abd017f86a9e
Tajik <tajik.ifrad@gmail.com>
2022-04-28 14:35:12 +0300

    wss

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import {
+  WebSocketClient,
+  WebSocketServer,
+} from "https://deno.land/x/websocket@v0.1.4/mod.ts";
 
 const app = new Application();
 
@@ -29,6 +33,14 @@ router
       context.response.body = books.get(context.params.id);
     }
   });
+
+const wss = new WebSocketServer(8080);
+wss.on("connection", function (ws: WebSocketClient) {
+  ws.on("message", function (message: string) {
+    console.log(message);
+    ws.send(message);
+  });
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
