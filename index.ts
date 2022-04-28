1feaef39863b926eb37c917d048c66e71489c342
Tajik <tajik.ifrad@gmail.com>
2022-04-28 13:52:33 +0300

    routes

---
+++ index.ts
@@ -0,0 +1,36 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+
+const app = new Application();
+
+app.use(
+  oakCors({
+    origin: "http://localhost:8080",
+  })
+);
+
+const books = new Map<string, any>();
+books.set("1", {
+  id: "1",
+  title: "The Hound of the Baskervilles",
+  author: "Conan Doyle, Arthur",
+});
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world of Conan!";
+  })
+  .get("/book", (context) => {
+    context.response.body = Array.from(books.values());
+  })
+  .get("/book/:id", (context) => {
+    if (books.has(context?.params?.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
